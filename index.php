19fdf6b63190b67aa0d61da4c8ea2d7df1859331
d309738 <d309738@edu.curio.nl>
2025-04-03 14:46:06 +0200

    index and main.css fix

--- index.php
+++ index.php
@@ -7,9 +7,9 @@
 </head>
 
 <body>
-<?php require_once 'main.php'; ?>
-<?php require_once('footer.php'); ?>
-<script src="https://kit.fontawesome.com/d6519435b0.js" crossorigin="anonymous"></script>
+    <?php require_once 'main.php'; ?>
+    <?php require_once('footer.php'); ?>
+    <script src="https://kit.fontawesome.com/d6519435b0.js" crossorigin="anonymous"></script>
 
 
 </body>
